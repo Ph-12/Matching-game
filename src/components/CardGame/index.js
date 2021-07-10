57c73702016624231613e40df0270202d47ed09c
Ph <sleephil@gmail.com>
2021-07-10 14:22:09 -0300

    Initial commit

---
+++ src/components/CardGame/index.js
@@ -0,0 +1,10 @@
+import "./style.css";
+function CardGame() {
+     return /*html*/ `
+        <article class="card-game">
+            <img src="img/alura-pixel.png" alt="logo alura">
+        </article>
+    `;
+}
+
+export default CardGame;
